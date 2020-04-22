ba02b2be5e80115e3eb12db950d004991437bf16
USER <user@USERnoMacBook-ea.local>
2020-04-22 11:47:12 +0900

    second commit

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const el = document.querySelector('.animate-title');
-    const str = el.innerHTML.trim();
-    let concatStr = '';
+    const str = el.innerHTML.trim().split("");
+
+    // let concatStr = '';
     
-    for(let c of str) {
-        c = c.replace(' ', '&nbsp;');
-        concatStr += `<span class="char">${c}</span>`;
-    }
-    el.innerHTML = concatStr;
+    // for(let c of str) {
+    //     c = c.replace(' ', '&nbsp;');
+    //     concatStr += `<span class="char">${c}</span>`;
+    // }
+
+    el.innerHTML = str.reduse((acc, curr) => {
+        curr = curr.replace(/\s+/, '&nbsp;');
+        return `${acc}<span class="char">${curr}</span>`;
+    }, "");
 });
